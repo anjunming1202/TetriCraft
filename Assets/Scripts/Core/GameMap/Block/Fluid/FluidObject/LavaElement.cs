de230255175f244115f1b466de690d584f3569a1
anjunming1202 <anjunming0819@outlook.com>
2026-06-21 03:12:20 +0100

    Adjust flame params; remove Lava TNT ignition
    Update flame behavior in GameplayLocal2P.unity: lower spreadRateMultiplier (3 -> 2.5), burnRateMultiplier (0.5 -> 0.4), and set spreadImmediateChance (0.5 -> 0) for both instances. Remove TNT-ignition behavior from LavaElement.cs by deleting OnUpdate and TryIgniteTNT (lava no longer ignites adjacent TNT) and removing unused usings.

--- Assets/Scripts/Core/GameMap/Block/Fluid/FluidObject/LavaElement.cs
+++ Assets/Scripts/Core/GameMap/Block/Fluid/FluidObject/LavaElement.cs
@@ -1,27 +1,3 @@
-using System.Collections.Generic;
-using UnityEngine;
-
 public class LavaElement : FluidElement
 {
-    public override void OnUpdate()
-    {
-        // ignite tnt
-        if (localLowerLevel == 0)
-            TryIgniteTNT(column, lowerGridPosition - 1);
-        for (int y = lowerGridPosition; y <= upperGridPosition; y++)
-        {
-            TryIgniteTNT(column - 1, y);
-            TryIgniteTNT(column + 1, y);
-        }
-        if (localUpperLevel == 0)
-            TryIgniteTNT(column, upperGridPosition + 1);
-    }
-
-    private void TryIgniteTNT(int x, int y)
-    {
-        if (map.GetBlock(x, y) is TNT target)
-        {
-            target.Ignite();
-        }
-    }
 }
